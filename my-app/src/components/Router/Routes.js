1234cbccc656515c48e9ed8fcca3a8fe6cc6d9a3
Daniel <danieletzinger@gmail.com>
2022-05-01 11:30:56 +1200

    Added router

--- my-app/src/components/Router/Routes.js
+++ my-app/src/components/Router/Routes.js
@@ -1,1 +1,24 @@
-// The Router Network that links the paces to their respective web adresses
+// The Router Network that links the paces to their respective web addresses
+
+import React, { Component } from "react";
+import { Router, Switch, Route } from "react-router-dom";
+
+//The Default Landing Page
+import Home from "./pages/Home";
+
+//Other Pages
+import Interpreter from "./pages/Interpreter";
+
+
+export default class Routes extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/Interpreter" component={Interpreter} />
+                </Switch>
+            </Router>
+        )
+    }
+}
